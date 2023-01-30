38f06c6fdecae5eab4325ac8688bd38038fa0104
omerkars78 <81978907+omerkars78@users.noreply.github.com>
2023-01-30 21:23:00 +0300

    dsds
    sdssd

--- controllers/user.js
+++ controllers/user.js
@@ -1,118 +1,106 @@
 const Blog = require("../models/blog");
-const Category = require("../models/category");y
+const Category = require("../models/category");
 const Contact = require("../models/contact");
 const About = require("../models/about");
 const { Op } = require("sequelize");
 
-exports.contacts = async function(req, res) {
-    console.log(req.cookies);
-    
+exports.contacts = async function (req, res) {
     try {
-        const contacts = await Contact.findAll({ raw: true });
-         res.render("users/contacts", {
-            title: "İletişim Bilgileri",
-            contacts:contacts
-        })
+      const contacts = await Contact.findAll({ raw: true });
+      res.render("users/contacts", {
+        title: "İletişim Bilgileri",
+        contacts,
+      });
+    } catch (error) {
+      console.error(error);
     }
-    catch(err) {
-        console.log(err);
+  };
+exports.about = async function (req, res) {
+  console.log(req.cookies);
+
+  try {
+    const about = await About.findAll();
+    if (about) {
+      return res.render("users/about", {
+        title: "Hakkımda",
+        about: about,
+      });
     }
-    
-}
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-exports.about = async function(req, res) {
-    console.log(req.cookies);
-    
-    try {
-        const about = await About.findAll();
-         res.render("users/about", {
-            title: "Hakkımda",
-            about:about
-        })
+exports.blogs_details = async function (req, res) {
+  const slug = req.params.slug;
+  try {
+    const blog = await Blog.findOne({
+      where: {
+        url: slug,
+      },
+      raw: true,
+    });
+
+    if (blog) {
+      return res.render("users/blog-details", {
+        title: blog.baslik,
+        blog: blog,
+      });
     }
-    catch(err) {
-        console.log(err);
-    }
-} 
+    res.redirect("/404");
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-exports.blogs_details = async function(req, res) {
-    const slug = req.params.slug;
-    try {
-        const blog = await Blog.findOne({
-            where: {
-                url: slug
-            },
-            raw: true
-        });
+exports.blog_list = async function (req, res) {
+  const size = 3;
+  const { page = 0 } = req.query;
+  const slug = req.params.slug;
 
-        if(blog) {
-            return res.render("users/blog-details", {
-                title: blog.baslik,
-                blog: blog
-            });
-        }
-        res.redirect("/404");
-    }
-    catch(err) {
-        console.log(err);
-    }
-}
+  try {
+    const { rows, count } = await Blog.findAndCountAll({
+      where: { onay: { [Op.eq]: true } },
+      raw: true,
+      include: slug ? { model: Category, where: { url: slug } } : null,
+      limit: size,
+      offset: page * size,
+    });
 
-exports.blog_list = async function(req, res) {
-    const size = 3;
-    const { page = 0 } = req.query;
-    const slug = req.params.slug;
+    const categories = await Category.findAll({ raw: true });
 
-    try {
-        const { rows, count } = await Blog.findAndCountAll({ 
-            where: { onay: {[Op.eq]: true } },
-            raw: true,
-            include: slug ? { model: Category, where: { url: slug } } : null,
-            limit: size,
-            offset: page * size 
-        });
+    res.render("users/blogs", {
+      title: "Tüm Kurslar",
+      blogs: rows,
+      totalItems: count,
+      totalPages: Math.ceil(count / size),
+      currentPage: page,
+      categories: categories,
+      selectedCategory: slug,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-        const categories = await Category.findAll({ raw: true });
+exports.index = async function (req, res) {
+  console.log(req.cookies);
+  try {
+    const blogs = await Blog.findAll({
+      where: {
+        [Op.and]: [{ anasayfa: true }, { onay: true }],
+      },
+      raw: true,
+    });
+    const categories = await Category.findAll({ raw: true });
 
-        res.render("users/blogs", {
-            title: "Tüm Kurslar",
-            blogs: rows,
-            totalItems: count,
-            totalPages: Math.ceil(count / size),
-            currentPage: page,
-            categories: categories,
-            selectedCategory: slug
-        })
-    }
-    catch(err) {
-        console.log(err);
-    }
-}
-
-exports.index = async function(req, res) {
-    console.log(req.cookies);
-    try {
-        const blogs = await Blog.findAll({
-            where: {
-                [Op.and]: [
-                    { anasayfa: true },
-                    { onay: true }
-                ]
-            },
-            raw: true
-        });
-        const categories = await Category.findAll({ raw: true });
-
-        res.render("users/index", {
-            title: "Popüler Kurslar",
-            blogs: blogs,
-            categories: categories,
-            selectedCategory: null
-        })
-    }
-    catch(err) {
-        console.log(err);
-    }
-}
-
-
+    res.render("users/index", {
+      title: "Popüler Kurslar",
+      blogs: blogs,
+      categories: categories,
+      selectedCategory: null,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
--- routes/user.js
+++ routes/user.js
@@ -7,14 +7,14 @@ router.get("/blogs/category/:slug", userController.blog_list);
 
 router.get("/blogs/:slug", userController.blogs_details);
 
-router.get("/blogs", userController.blog_list);
-
 router.get("/contacts", userController.contacts);
 
 router.get("/about", userController.about);
 
+router.get("/blogs", userController.blog_list);
+
 router.get("/", userController.index);
- 
+
 
 
 module.exports = router;

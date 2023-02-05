dcfe6ffc7c791574ab64e70f1ba009cb7b25fc0b
omerkars78 <81978907+omerkars78@users.noreply.github.com>
2023-02-05 21:30:50 +0300

    knkn
    knkn

--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const authController = require("../controllers/auth");
 const csrf = require("../middlewares/csrf");
 
-router.get("/register",csrf, authController.get_register);
-router.post("/register",csrf, authController.post_register);
+// router.get("/register",csrf, authController.get_register);
+// router.post("/register",csrf, authController.post_register);
 
 router.get("/login",csrf, authController.get_login);
 router.post("/login", authController.post_login);
